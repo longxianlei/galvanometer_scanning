61d46ccbf11a2ec797df3387fddf2065cc6f59fa
longxianlei <806028443lxl@gmail.com>
2021-05-12 22:22:29 +0800

    Reform the plot functions. Include average scanning routes and NumOfSample vs FPS, scanning time, total time. Newly.

--- plot_avg_routes.py
+++ plot_avg_routes.py
@@ -7,42 +7,45 @@ This part include 2 functions.
 import numpy as np
 import matplotlib.pyplot as plt
 
-"""
-1. Plot the average scan routes of 4 methods.
-"""
-avg_route_axis_scan = np.load('./generate_dataset/avg_routes_axis.npy')
-avg_route_2opt_scan = np.load('./generate_dataset/avg_routes_2opt.npy')
-avg_route_google_scan = np.load('./generate_dataset/avg_routes_google.npy')
-avg_route_defined_scan = np.load('./generate_dataset/avg_routes_assigned.npy')
 
-max_f = np.max([avg_route_google_scan, avg_route_2opt_scan, avg_route_axis_scan, avg_route_defined_scan])
-min_f = np.min([avg_route_google_scan, avg_route_2opt_scan, avg_route_axis_scan, avg_route_defined_scan])
-y_max = np.ceil(max_f)
-y_min = np.floor(min_f)
+def plot_avg_routes_01():
+    """
+    1. Plot the average scan routes of 4 methods.
+    :return:
+    """
+    avg_route_axis_scan = np.load('./generate_dataset/avg_routes_axis.npy')
+    avg_route_2opt_scan = np.load('./generate_dataset/avg_routes_2opt.npy')
+    avg_route_google_scan = np.load('./generate_dataset/avg_routes_google.npy')
+    avg_route_defined_scan = np.load('./generate_dataset/avg_routes_assigned.npy')
 
-len_data = len(avg_route_axis_scan)
-x = np.arange(1, len_data+1, 1)*10
+    max_f = np.max([avg_route_google_scan, avg_route_2opt_scan, avg_route_axis_scan, avg_route_defined_scan])
+    min_f = np.min([avg_route_google_scan, avg_route_2opt_scan, avg_route_axis_scan, avg_route_defined_scan])
+    y_max = np.ceil(max_f)
+    y_min = np.floor(min_f)
 
-plt.figure(1, dpi=150, figsize=(8, 6))
-# 改变文字大小参数-fontsize
-# 设置坐标轴的取值范围;
-plt.xlim((0, len_data*10))
-plt.ylim((y_min, y_max))
-# 设置坐标轴的label;
-plt.xlabel('Num of Samples', fontsize=15)
-plt.ylabel('Average length', fontsize=15)
-plt.title('The average routes for 4 methods', fontsize=15)
-# 设置x坐标轴刻度;
-plt.xticks(np.linspace(0, len_data*10, 11), fontsize=15)
-plt.yticks(np.linspace(y_min, y_max, 15), fontsize=15)
+    len_data = len(avg_route_axis_scan)
+    x = np.arange(1, len_data+1, 1)*10
 
-plt.plot(x, avg_route_axis_scan, '*--', label='X-axis Scan')
-plt.plot(x, avg_route_2opt_scan, 'g^-.', label='2opt Scan')
-plt.plot(x, avg_route_google_scan, 'yx-', label='Google Scan')
-plt.plot(x, avg_route_defined_scan, 'ro--', label='Own Scan')
-plt.legend(loc='best', fontsize=15)
-# plt.savefig('./experiment_results/4_methods_avg_routes_orig_test2.jpg')
-# plt.show()
+    plt.figure(1, dpi=150, figsize=(8, 6))
+    # 改变文字大小参数-fontsize
+    # 设置坐标轴的取值范围;
+    plt.xlim((0, len_data*10))
+    plt.ylim((y_min, y_max))
+    # 设置坐标轴的label;
+    plt.xlabel('Num of Samples', fontsize=15)
+    plt.ylabel('Average length', fontsize=15)
+    plt.title('The average routes for 4 methods', fontsize=15)
+    # 设置x坐标轴刻度;
+    plt.xticks(np.linspace(0, len_data*10, 11), fontsize=15)
+    plt.yticks(np.linspace(y_min, y_max, 15), fontsize=15)
+
+    plt.plot(x, avg_route_axis_scan, '*--', label='X-axis Scan')
+    plt.plot(x, avg_route_2opt_scan, 'g^-.', label='2opt Scan')
+    plt.plot(x, avg_route_google_scan, 'yx-', label='Google Scan')
+    plt.plot(x, avg_route_defined_scan, 'ro--', label='Own Scan')
+    plt.legend(loc='best', fontsize=15)
+    # plt.savefig('./experiment_results/4_methods_avg_routes_orig_test2.jpg')
+    # plt.show()
 
 
 """
@@ -60,99 +63,109 @@ len_data = len(axis_scan)
 x = np.arange(1, len_data+1, 1)*50
 
 
-"""
-2.1, Plot the FPS_NumOfSamples curve. 
-(PS: The more samples, each route is shorter, then FPS is faster.)
-"""
-# plt.figure('average_scan_routes', figsize=(10, 8), dpi=80)
-plt.figure(2, dpi=150, figsize=(8, 6))
-# 改变文字大小参数-fontsize
-# 设置坐标轴的取值范围;
-plt.xlim((0, len_data*50))
-plt.ylim((150, 300))
-# 设置坐标轴的label;
-plt.xlabel('Num of Samples', fontsize=15)
-plt.ylabel('Average scanning FPS', fontsize=15)
-plt.title('The average scanning speed', fontsize=15)
-# 设置x坐标轴刻度;
-plt.xticks(np.linspace(0, len_data*50, 11), fontsize=15)
-plt.yticks(np.linspace(150, 300, 16), fontsize=15)
+def plot_avg_scanning_fps_02():
+    """
+    2.1, Plot the FPS_NumOfSamples curve.
+    (PS: The more samples, each route is shorter, then FPS is faster.)
+    """
+    # plt.figure('average_scan_routes', figsize=(10, 8), dpi=80)
+    plt.figure(2, dpi=150, figsize=(8, 6))
+    # 改变文字大小参数-fontsize
+    # 设置坐标轴的取值范围;
+    plt.xlim((0, len_data*50))
+    plt.ylim((150, 300))
+    # 设置坐标轴的label;
+    plt.xlabel('Num of Samples', fontsize=15)
+    plt.ylabel('Average scanning FPS', fontsize=15)
+    plt.title('The average scanning speed', fontsize=15)
+    # 设置x坐标轴刻度;
+    plt.xticks(np.linspace(0, len_data*50, 11), fontsize=15)
+    plt.yticks(np.linspace(150, 300, 16), fontsize=15)
 
-plt.plot(x, 1000.0/np.asarray(axis_scan), '*--', label='X-axis Scan')
-plt.plot(x, 1000.0/np.asarray(two_opt_scan), 'g^-.', label='2opt Scan')
-plt.plot(x, 1000.0/np.asarray(google_scan), 'yx-', label='Google Scan')
-plt.plot(x, 1000.0/np.asarray(own_scan), 'ro--', label='Own Scan')
+    plt.plot(x, 1000.0/np.asarray(axis_scan), '*--', label='X-axis Scan')
+    plt.plot(x, 1000.0/np.asarray(two_opt_scan), 'g^-.', label='2opt Scan')
+    plt.plot(x, 1000.0/np.asarray(google_scan), 'yx-', label='Google Scan')
+    plt.plot(x, 1000.0/np.asarray(own_scan), 'ro--', label='Own Scan')
 
-plt.legend(loc='best', fontsize=15)
-# plt.savefig('./analysis_fig/scan_routes_speed_comparison/4_methods_avg_scanning_fps.jpg')
-# plt.show()
-
-
-"""
-2.2, Plot the average scanning time V.S. the number of samples.
-(PS: The more samples, each route is shorter, the average scanning time of each point is less.)
-"""
-# plt.figure('average_scan_routes', figsize=(10, 8), dpi=80)
-plt.figure(3, dpi=150, figsize=(8, 6))
-# 改变文字大小参数-fontsize
-# 设置坐标轴的取值范围;
-plt.xlim((0, len_data*50))
-plt.ylim((3.00, 6.50))
-# 设置坐标轴的label;
-plt.xlabel('Num of Samples', fontsize=15)
-plt.ylabel('Average scanning times: (ms)', fontsize=15)
-plt.title('The average scanning speed', fontsize=15)
-# 设置x坐标轴刻度;
-plt.xticks(np.linspace(0, len_data*50, 11), fontsize=15)
-plt.yticks(np.linspace(3.00, 6.50, 15), fontsize=15)
-
-plt.plot(x, axis_scan, '*--', label='X-axis Scan')
-plt.plot(x, two_opt_scan, 'g^-.', label='2opt Scan')
-plt.plot(x, google_scan, 'yx-', label='Google Scan')
-plt.plot(x, own_scan, 'ro--', label='Own Scan')
-plt.legend(loc='best', fontsize=15)
-# plt.savefig('./analysis_fig/scan_routes_speed_comparison/4_methods_avg_scanning_fps.jpg')
-# plt.show()
+    plt.legend(loc='best', fontsize=15)
+    # plt.savefig('./analysis_fig/scan_routes_speed_comparison/4_methods_avg_scanning_fps.jpg')
+    # plt.show()
 
 
-"""
-2.3, Plot the total scanning time V.S. the number of samples.
-(The more samples, the totally scanning time of the whole samples are larger.
-"""
-# plt.figure('average_scan_routes', figsize=(10, 8), dpi=80)
-plt.figure(4, dpi=150, figsize=(8, 6))
-# 改变文字大小参数-fontsize
-# 设置坐标轴的取值范围;
-plt.xlim((0, len_data*50))
-# 设置坐标轴的label;
-plt.xlabel('Num of Samples', fontsize=15)
-plt.ylabel('Scanning times: (ms)', fontsize=15)
-plt.title('The total scanning times of 4 methods', fontsize=15)
-# 设置x坐标轴刻度;
-plt.xticks(np.linspace(0, len_data*50, 11), fontsize=15)
+def plot_avg_scanning_time_03():
+    """
+    2.2, Plot the average scanning time V.S. the number of samples.
+    (PS: The more samples, each route is shorter, the average scanning time of each point is less.)
+    """
+    # plt.figure('average_scan_routes', figsize=(10, 8), dpi=80)
+    plt.figure(3, dpi=150, figsize=(8, 6))
+    # 改变文字大小参数-fontsize
+    # 设置坐标轴的取值范围;
+    plt.xlim((0, len_data*50))
+    plt.ylim((3.00, 6.50))
+    # 设置坐标轴的label;
+    plt.xlabel('Num of Samples', fontsize=15)
+    plt.ylabel('Average scanning times: (ms)', fontsize=15)
+    plt.title('The average scanning speed', fontsize=15)
+    # 设置x坐标轴刻度;
+    plt.xticks(np.linspace(0, len_data*50, 11), fontsize=15)
+    plt.yticks(np.linspace(3.00, 6.50, 15), fontsize=15)
 
-# calculate the total scanning times.
-axis_total_scan_time = []
-for i in range(len(axis_scan)):
-    axis_total_scan_time.append(np.round((50*(i+1)*axis_scan[i]), 4))
-print(axis_total_scan_time)
+    plt.plot(x, axis_scan, '*--', label='X-axis Scan')
+    plt.plot(x, two_opt_scan, 'g^-.', label='2opt Scan')
+    plt.plot(x, google_scan, 'yx-', label='Google Scan')
+    plt.plot(x, own_scan, 'ro--', label='Own Scan')
+    plt.legend(loc='best', fontsize=15)
+    # plt.savefig('./analysis_fig/scan_routes_speed_comparison/4_methods_avg_scanning_fps.jpg')
+    # plt.show()
 
-two_opt_total_scan_time = []
-for i in range(len(axis_scan)):
-    two_opt_total_scan_time.append(np.round((50*(i+1)*two_opt_scan[i]), 4))
 
-google_total_scan_time = []
-for i in range(len(axis_scan)):
-    google_total_scan_time.append(np.round((50*(i+1)*google_scan[i]), 4))
+def plot_total_scanning_time_04():
+    """
+    2.3, Plot the total scanning time V.S. the number of samples.
+    (The more samples, the totally scanning time of the whole samples are larger.
+    """
+    # plt.figure('average_scan_routes', figsize=(10, 8), dpi=80)
+    plt.figure(4, dpi=150, figsize=(8, 6))
+    # 改变文字大小参数-fontsize
+    # 设置坐标轴的取值范围;
+    plt.xlim((0, len_data*50))
+    # 设置坐标轴的label;
+    plt.xlabel('Num of Samples', fontsize=15)
+    plt.ylabel('Scanning times: (ms)', fontsize=15)
+    plt.title('The total scanning times of 4 methods', fontsize=15)
+    # 设置x坐标轴刻度;
+    plt.xticks(np.linspace(0, len_data*50, 11), fontsize=15)
 
-own_total_scan_time = []
-for i in range(len(axis_scan)):
-    own_total_scan_time.append(np.round((50*(i+1)*own_scan[i]), 4))
+    # calculate the total scanning times.
+    axis_total_scan_time = []
+    for i in range(len(axis_scan)):
+        axis_total_scan_time.append(np.round((50*(i+1)*axis_scan[i]), 4))
+    print(axis_total_scan_time)
 
-plt.plot(x, axis_total_scan_time, '*--', label='X-axis Scan')
-plt.plot(x, two_opt_total_scan_time, 'g^-.', label='2opt Scan')
-plt.plot(x, google_total_scan_time, 'yx-', label='Google Scan')
-plt.plot(x, own_total_scan_time, 'ro--', label='Own Scan')
-plt.legend(loc='best', fontsize=15)
-# plt.savefig('./analysis_fig/scan_routes_speed_comparison/4_methods_total_scanning_time.jpg')
-plt.show()
+    two_opt_total_scan_time = []
+    for i in range(len(axis_scan)):
+        two_opt_total_scan_time.append(np.round((50*(i+1)*two_opt_scan[i]), 4))
+
+    google_total_scan_time = []
+    for i in range(len(axis_scan)):
+        google_total_scan_time.append(np.round((50*(i+1)*google_scan[i]), 4))
+
+    own_total_scan_time = []
+    for i in range(len(axis_scan)):
+        own_total_scan_time.append(np.round((50*(i+1)*own_scan[i]), 4))
+
+    plt.plot(x, axis_total_scan_time, '*--', label='X-axis Scan')
+    plt.plot(x, two_opt_total_scan_time, 'g^-.', label='2opt Scan')
+    plt.plot(x, google_total_scan_time, 'yx-', label='Google Scan')
+    plt.plot(x, own_total_scan_time, 'ro--', label='Own Scan')
+    plt.legend(loc='best', fontsize=15)
+    # plt.savefig('./analysis_fig/scan_routes_speed_comparison/4_methods_total_scanning_time.jpg')
+    plt.show()
+
+
+if __name__ == '__main__':
+    plot_avg_routes_01()
+    plot_avg_scanning_fps_02()
+    plot_avg_scanning_time_03()
+    plot_total_scanning_time_04()
