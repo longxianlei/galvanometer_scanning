4bb8e64aabf5eda97e66fc45c17494bff8c217bf
longxianlei <806028443lxl@gmail.com>
2021-05-12 20:16:09 +0800

    Reform the plot functions. Include average scanning routes and NumOfSample vs FPS, scanning time, total time.

--- density_scan_experiments.py
+++ density_scan_experiments.py
@@ -7,8 +7,8 @@ import matplotlib.pyplot as plt
 from draft import cal_distance, gen_scan_order, two_opt_solver, TSPSolverGoogle
 np.random.seed(0)
 
-# First, compare x-axis based sorted-scan and optimized google solver-scan.
-# The metric include statistical average length of points, average scan time of points.
+# First, compare x-axis based sorted-scan，2opt-scan and optimized google solver-scan.
+# The metric include statistical average routes of scanning points, average scan time of points.
 
 
 def generate_scan_samples():
@@ -40,7 +40,6 @@ def generate_scan_samples():
 
         # whether specify the start/end point. If TRUE, given the two determined points.
         is_defined_start_end = True
-
         add_all = ctrl_vxs + ctrl_vys
         index_max = np.argmax(add_all)
         index_min = np.argmin(add_all)
@@ -186,12 +185,148 @@ def load_scan_routes(routes_name):
 if __name__ == '__main__':
     # The name of the 3 scan routes are: axis_based_scan_routes, two_opt_scan_routes, google_scan_routes
     # self_load = np.load('axis_based_scan_routes.npy', allow_pickle=True)
-    # axis_scan_x, axis_scan_y, axis_scan_nums = load_scan_routes('axis_based_scan_routes.npy')
-    # topt_scan_x, topt_scan_y, topt_scan_nums = load_scan_routes('two_opt_scan_routes.npy')
-    # own_scan_x, own_scan_y, own_scan_nums = load_scan_routes('google_scan_routes.npy')
+    axis_scan_x, axis_scan_y, axis_scan_nums = load_scan_routes('axis_based_scan_routes.npy')
+    topt_scan_x, topt_scan_y, topt_scan_nums = load_scan_routes('two_opt_scan_routes.npy')
+    google_scan_x, google_scan_y, google_scan_nums = load_scan_routes('google_scan_routes.npy')
+    own_scan_x, own_scan_y, own_scan_nums = load_scan_routes('defined_points_scan_routes.npy')
+
+    # print(axis_scan_nums, topt_scan_nums, google_scan_nums, own_scan_nums)
+    print(google_scan_x)
     # test1=np.load('avg_routes_assigned.npy')
     # print(test1)
     # test2 = np.load('defined_points_scan_routes.npy', allow_pickle=True)
     # print(test2.item().get('x'))
-    generate_scan_samples()
+    # generate_scan_samples()
+
+    """
+    total_samples = 100
+    data_x = np.round(np.random.uniform(low=0.0, high=1.0, size=total_samples), 4)
+    data_y = np.round(np.random.uniform(low=0.0, high=1.0, size=total_samples), 4)
+    scan_x = []
+    scan_y = []
+    index = 0
+    # print(data_x)
+    # print(data_y)
+    for i in range(10):
+        for j in range(10):
+            if i % 2 == 0:
+                scan_x.append((j-5)+data_x[index])
+                scan_y.append((i - 5) + data_y[index])
+            else:
+                scan_x.append(-((j-5)+data_x[index]))
+                scan_y.append((i - 5) + data_y[index])
+            index += 1
+    # print(scan_x)
+    # print(scan_y)
+    scan_x = np.array(scan_x)
+    scan_y = np.array(scan_y)
+
+    order_scan_x, order_scan_y, order_scan_routes = gen_scan_order(scan_x, scan_y, total_points=total_samples)
+
+    distance_matrix = cal_distance(scan_x, scan_y)
+    scaled_distance_matrix = distance_matrix * 1000
+    after_x, after_y, cal_dist = gen_scan_order(scan_x, scan_y,
+                                                    total_points=total_samples, separate_grid=20)
+
+    two_opt_x, two_opt_y, two_opt_dist = two_opt_solver(scan_x, scan_y, scaled_distance_matrix,
+                                                            total_points=total_samples)
+
+    google_solver = TSPSolverGoogle(scan_x, scan_y, scaled_distance_matrix, total_points=total_samples,
+                                    is_defined_points=False,
+                                    start_point=6, end_point=9)
+    google_x, google_y, google_dist = google_solver.solve_travel()
+
+    add_all = scan_x + scan_y
+    index_max = np.argmax(add_all)
+    index_min = np.argmin(add_all)
+    START_POINT = index_min
+    END_POINT = index_max
+
+    google_solver = TSPSolverGoogle(scan_x, scan_y, scaled_distance_matrix, total_points=total_samples,
+                                    is_defined_points=True,
+                                    start_point=int(START_POINT), end_point=int(END_POINT))
+    own_x, own_y, own_dist = google_solver.solve_travel()
+
+
+    print('avg_routes: ', order_scan_routes)
+    plt.figure(3)
+    # 设置坐标轴的取值范围;
+    plt.xlim((-5, 5))
+    plt.ylim((-5, 5))
+    # 设置坐标轴的label;
+    plt.xlabel('X voltage')
+    plt.ylabel('Y voltage')
+    plt.title('Grid Based Scan:' + str(np.round(order_scan_routes/total_samples, 4)))
+    # 设置x坐标轴刻度;
+    plt.xticks(np.linspace(-5, 5, 11))
+    plt.yticks(np.linspace(-5, 5, 11))
+
+    plt.plot(scan_x, scan_y, '*-')
+    plt.savefig('1_grid_based_scan.jpg')
+    # plt.show()
+
+    plt.figure(4)
+    # 设置坐标轴的取值范围;
+    plt.xlim((-5, 5))
+    plt.ylim((-5, 5))
+    # 设置坐标轴的label;
+    plt.xlabel('X voltage')
+    plt.ylabel('Y voltage')
+    plt.title('X-axis Order Based Scan: ' + str(np.round(cal_dist/total_samples, 4)))
+    # 设置x坐标轴刻度;
+    plt.xticks(np.linspace(-5, 5, 11))
+    plt.yticks(np.linspace(-5, 5, 11))
+    plt.plot(after_x, after_y, 'mo-.')
+    plt.savefig('1_X-axis_based_Scan_grid_20.jpg')
+
+    plt.figure(5)
+    # 设置坐标轴的取值范围;
+    plt.xlim((-5, 5))
+    plt.ylim((-5, 5))
+    # 设置坐标轴的label;
+    plt.xlabel('X voltage')
+    plt.ylabel('Y voltage')
+    plt.title('2opt Based Scan: ' + str(np.round(two_opt_dist/total_samples, 4)))
+    # 设置x坐标轴刻度;
+    plt.xticks(np.linspace(-5, 5, 11))
+    plt.yticks(np.linspace(-5, 5, 11))
+    plt.plot(two_opt_x, two_opt_y, 'b*--')
+    plt.plot(two_opt_x[0], two_opt_y[0], 'r*')
+    plt.plot(two_opt_x[-1], two_opt_y[-1], 'r*')
+    plt.savefig('1_two_opt_based_scan.jpg')
+
+    plt.figure(6)
+    # 设置坐标轴的取值范围;
+    plt.xlim((-5, 5))
+    plt.ylim((-5, 5))
+    # 设置坐标轴的label;
+    plt.xlabel('X voltage')
+    plt.ylabel('Y voltage')
+    plt.title('Google Based Scan: ' + str(np.round(google_dist/total_samples, 4)))
+    # 设置x坐标轴刻度;
+    plt.xticks(np.linspace(-5, 5, 11))
+    plt.yticks(np.linspace(-5, 5, 11))
+    plt.plot(google_x, google_y, 'y^--')
+    plt.plot(google_x[0], google_y[0], 'r*')
+    plt.plot(google_x[-1], google_y[-1], 'r*')
+    plt.savefig('1_google_based_scan_new.jpg')
+
+    plt.figure(7)
+    # 设置坐标轴的取值范围;
+    plt.xlim((-5, 5))
+    plt.ylim((-5, 5))
+    # 设置坐标轴的label;
+    plt.xlabel('X voltage')
+    plt.ylabel('Y voltage')
+    plt.title('Our solver Based Scan: ' + str(np.round(own_dist/total_samples, 4)))
+    # 设置x坐标轴刻度;
+    plt.xticks(np.linspace(-5, 5, 11))
+    plt.yticks(np.linspace(-5, 5, 11))
+    plt.plot(own_x, own_y, 'g^--')
+    plt.plot(scan_x[START_POINT], scan_y[START_POINT], 'r*')
+    plt.plot(scan_x[END_POINT], scan_y[END_POINT], 'r*')
+    plt.savefig('1_own_solver_based_scan.jpg')
+    plt.show()
+    """
+
 
