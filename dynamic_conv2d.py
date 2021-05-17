223c70c07e1e5a425454c59106e1f6e0359e7809
longxianlei <806028443lxl@gmail.com>
2021-05-17 21:24:05 +0800

    Dynamic convolution, this the basic implementation of DY-Conv.

---
+++ dynamic_conv2d.py
@@ -0,0 +1,59 @@
+import torch.nn as nn
+import torch as torch
+import torch.nn.functional as F
+
+
+class Attention2D(nn.Module):
+    def __init__(self, in_planes, K,):
+        super(Attention2D, self).__init__()
+        self.avgpool = nn.AdaptiveAvgPool2d(2)
+        self.fc1 = nn.Conv2d(in_planes, K, 1,)
+        self.fc2 = nn.Conv2d(K, K, 1,)
+
+    def forward(self, x):
+        x = self.avgpool(x)
+        x = self.fc1(x)
+        x = nn.Relu(x)
+        x = self.fc2(x).view(x.size(0), -1)
+        return nn.Softmax(x, 1)
+
+
+class DynamicConv2d(nn.Module):
+    def __init__(self, in_planes, out_planes, kernel_size, stride=1, padding=0, dilation=1, groups=1, bias=True, K=4,):
+        super(DynamicConv2d, self).__init__()
+        assert in_planes % groups == 0
+        self.in_planes = in_planes
+        self.out_planes = out_planes
+        self.kernel_size = kernel_size
+        self.stride = stride
+        self.padding = padding
+        self.dilation = dilation
+        self.groups = groups
+        self.bias = bias
+        self.K = K
+        self.attention = Attention2D(in_planes, K, )
+        self.weight = nn.Parameter(torch.Tensor(K, out_planes, in_planes//groups, kernel_size, kernel_size),
+                                   requires_grad=True)
+        if bias:
+            self.bias = nn.Parameter(torch.Tensor(K, out_planes))
+        else:
+            self.bias = None
+
+    def forward(self, x):
+        softmax_attention = self.attention(x)
+        batch_size, in_planes, height, width = x.size()
+        x = x.view(1, -1, height, width)
+        weight = self.weight.view(self.K, -1)
+        aggregate_weight = torch.mm(softmax_attention, weight).view(self.out_planes, -1,
+                                                                     self.kernel_size, self.kernel_size)
+        if self.bias is not None:
+            aggregate_bias = torch.mm(softmax_attention, self.bias).view(-1)
+            output = F.conv2d(x, weight=aggregate_weight, bias=aggregate_bias, stride=self.stride, padding=self.padding,
+                              dilation=self.dilation, groups=self.groups * batch_size)
+        else:
+            output = F.conv2d(x, weight=aggregate_weight, bias=None, stride=self.stride, padding=self.padding,
+                              dilation=self.dilation, groups=self.groups * batch_size)
+
+        output = output.view(batch_size, self.out_planes, output.size(-2), output.size(-1))
+        return output
+
