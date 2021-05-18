b28c60ae3ac4ce47d4292785d8805222a65e048e
longxianlei <806028443lxl@gmail.com>
2021-05-18 23:07:23 +0800

    Implementation of the sampling based on the object points.

---
+++ resample_filter.py
@@ -0,0 +1,7 @@
+import random
+import numpy as np
+
+"""
+This part is intended to solve the sampling based re_sample method. When there are objects in the sample image, 
+we should consider sampling/pay more attention in the neighborhood of this sample. 
+"""
